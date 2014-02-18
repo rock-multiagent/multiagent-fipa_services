41251cf27cfd6891bd40c77f51d9b73284126c0d
Thomas Roehr <thomas.roehr@dfki.de>
2014-02-18 17:49:15 +0100

    Add ArgumentError definition

--- src/ErrorHandling.hpp
+++ src/ErrorHandling.hpp
@@ -19,6 +19,7 @@ namespace services {
 FIPA_SERVICE_EXCEPTION(DuplicateEntry, "Entry already exists: ")
 FIPA_SERVICE_EXCEPTION(NotFound, "Entry could not be found: ")
 FIPA_SERVICE_EXCEPTION(NotImplemented, "Function has not been implemeted yet: ")
+FIPA_SERVICE_EXCEPTION(ArgumentError, "Invalid argument provided: ")
 
 } // end namespace services
 } // end namespace fipa
