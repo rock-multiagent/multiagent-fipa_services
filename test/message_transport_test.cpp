23c6b0adb5a5518df5293061739a9ca5d9a07e22
Thomas Roehr <thomas.roehr@dfki.de>
2015-02-13 11:31:42 +0100

    test: update message_transport test for nominal and error handling case

--- test/message_transport_test.cpp
+++ test/message_transport_test.cpp
@@ -62,39 +62,18 @@ BOOST_AUTO_TEST_CASE(inter_service_communication)
     messageTransport0.activateTransport(transports::Transport::UDT);
     messageTransport1.activateTransport(transports::Transport::UDT);
 
-    messageTransport0.setTransportEndpoints("usb0");
-    messageTransport1.setTransportEndpoints("usb0");
-
     AgentID mt0Client("mt0-client");
     AgentID mt1Client("mt1-client");
 
     // Setting up the proper entries in the service directory
     {
-        ServiceLocator locator;
-        std::vector<fipa::services::ServiceLocation> locations = messageTransport0.getTransportEndpoints();
-        for(std::vector<fipa::services::ServiceLocation>::const_iterator it = locations.begin();
-            it != locations.end(); it++)
-        {
-            locator.addLocation(*it);
-        }
-
         std::string description = "Message client of " + messageTransport0.getAgentID().getName();
-        fipa::services::ServiceDirectoryEntry client(mt0Client.getName(), "fipa::services::MessageTransport", locator, description);
-        serviceDirectory->registerService(client);
+        messageTransport0.registerClient(mt0Client.getName(), description);
     }
 
     {
-        ServiceLocator locator;
-        std::vector<fipa::services::ServiceLocation> locations = messageTransport1.getTransportEndpoints();
-        for(std::vector<fipa::services::ServiceLocation>::const_iterator it = locations.begin();
-            it != locations.end(); it++)
-        {
-            locator.addLocation(*it);
-        }
-
         std::string description = "Message client of " + messageTransport1.getAgentID().getName();
-        fipa::services::ServiceDirectoryEntry client(mt1Client.getName(), "fipa::services::MessageTransport", locator, description);
-        serviceDirectory->registerService(client);
+        messageTransport1.registerClient(mt1Client.getName(), description);
     }
 
     TestDelivery delivery;
@@ -119,4 +98,53 @@ BOOST_AUTO_TEST_CASE(inter_service_communication)
     }
 }
 
+BOOST_AUTO_TEST_CASE(inter_service_communication_fail)
+{
+    using namespace fipa::acl;
+    using namespace fipa::services::message_transport;
+    using namespace fipa::services;
+
+    ServiceDirectory::Ptr serviceDirectory(new ServiceDirectory());
+
+    MessageTransport messageTransport0(AgentID("mts-0"), serviceDirectory);
+    MessageTransport messageTransport1(AgentID("mts-1"), serviceDirectory);
+
+    messageTransport0.activateTransport(transports::Transport::UDT);
+    messageTransport1.activateTransport(transports::Transport::UDT);
+
+    AgentID mt0Client("mt0-client");
+    AgentID mt1Client("mt1-client");
+
+    // Setting up the proper entries in the service directory
+    {
+        std::string description = "Message client of " + messageTransport0.getAgentID().getName();
+        messageTransport0.registerClient(mt0Client.getName(), description);
+    }
+
+    {
+        std::string description = "Message client of " + messageTransport1.getAgentID().getName();
+        messageTransport1.registerClient(mt1Client.getName(), description);
+    }
+
+    TestDelivery delivery;
+    messageTransport0.registerMessageTransport("default-corba-transport", boost::bind(&TestDelivery::deliverOrForwardLetterSuccess,delivery,_1,_2));
+    messageTransport1.registerMessageTransport("default-corba-transport", boost::bind(&TestDelivery::deliverOrForwardLetterFail,delivery,_1,_2));
+
+    ACLMessage msg;
+    msg.setSender(mt0Client);
+    msg.addReceiver(mt1Client);
+    msg.setContent("Test Content");
+    ACLEnvelope env(msg, representation::BITEFFICIENT);
+    messageTransport0.handle(env);
+    //messageTransport1.handle(env);
+
+
+    for(int i = 0; i < 10; ++i)
+    {
+        BOOST_TEST_MESSAGE("Trigger");
+        messageTransport0.trigger();
+        messageTransport1.trigger();
+        sleep(0.5);
+    }
+}
 BOOST_AUTO_TEST_SUITE_END()
